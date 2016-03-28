7bc9d6c68405112be43f56834531ccc23eb5c49d
lily-xue <414564926@qq.com>
2016-03-28 07:58:14 -0700

    depot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dept::Application.config.secret_key_base = 'a38f078d918b497c4037c736345db50938afff47826e4b2294606508fbf370adb004de02ee3d6d86e7d39357539cdc9200ee0616686c02a67af9455979d503fd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dept::Application.config.session_store :cookie_store, key: '_dept_session'
