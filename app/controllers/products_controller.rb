60d36dc06e936477bbb8157544eb8ff3184875eb
lily-xue <414564926@qq.com>
2016-04-03 08:48:58 -0700

    添加购物车

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -64,8 +64,8 @@ class ProductsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
-      @product = Product.find(params[:id])
-    end
+   @product = Product.find(params[:id])
+   end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,40 @@
 class Product < ActiveRecord::Base
+default_scope :order => 'title'
+has_many :line_items
+before_destroy :ensure_not_referened_by_any_line_item
+validates :title,:description,:image_url,presence:true
+
+validates :price,:numericality => {:greater_than_or_equal_to => 0.01}
+validates :title,uniqueness: true
+
+validates :image_url,:format => {
+:with =>%r{\.(gif|jpg|png)}i,
+:messages => 'must be a url for gif,jpg or png image'}
+
+private 
+def ensure_not_referened_by_any_line_item
+if line_items.empty?
+return true
+else 
+errors.add(:base,'line items present')
+return false
 end
+end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
